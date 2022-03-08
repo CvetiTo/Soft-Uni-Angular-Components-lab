bc555b33446a669133013f9fb8884791052284cc
CvetiTo <email@example.com>
2022-03-08 17:05:01 +0200

    added Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    ArticlesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
